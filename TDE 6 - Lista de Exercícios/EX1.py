197b01e9b2fbeb43e1c61990c2af5e0783f179cc
At0micAnt <pedrodafonsecaschwertner@gmail.com>
2025-05-14 09:25:31 -0300

    TDE 6 started

--- TDE 6 - Lista de Exercícios/EX1.py
+++ TDE 6 - Lista de Exercícios/EX1.py
@@ -2,3 +2,24 @@
 # então coloque em um outro vetor de 4 posições o maior valor encontrado na coluna da matriz cujo
 # índice é o mesmo do vetor, ou seja, o maior valor da coluna zero da matriz na posição zero do vetor e
 # assim por diante. Mostre então a matriz, o vetor e a média aritmética do vetor.
+
+import random
+
+m = []
+v = []
+
+for i in range(0, 4):
+    linha_m = []
+    for j in range(0, 4):
+        linha_m.append(random.randint(0, 9))
+    m.append(linha_m)
+    maior = max(linha_m)
+    v.append(maior)
+
+print("Matriz: ")
+for i in range(len(m)):
+    for j in range(len(m[0])):
+        print(m[i][j], end = ' | ')
+    print()
+
+print(v)
